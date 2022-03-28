82f0267a3ce6767b1686b18e95a49e71be5eeeb7
valcyclovir <swaggrenox@gmail.com>
2022-03-28 15:18:09 -0400

    cleaned up ports

--- other-scripts/bulkpayout.sh
+++ other-scripts/bulkpayout.sh
@@ -13,14 +13,12 @@ source "$MAINPATH/data/fixed-variables.sh"
 
 for var
 do
-  PORT1=$((var+2999))
-  PORT2=$((var+5277))
-  PORT3=$((var+8899))
+  PORT=$((var+8899))
   NODE="$NODE_NAME$var"
   NODEID="NODE_ID_$var"
 
     curl -X GET "https://v5api.othub.info/api/nodes/DataHolder/${!NODEID}/jobs" -H "accept: text/plain" | jq -r '. - map(select(.Status | contains ("Active"))) | .[] .OfferId' > offerids.txt
 
-    cat offerids.txt | while read line; do docker exec $NODE curl -s -X GET http://127.0.0.1:$PORT3/api/latest/payout?offer_id=$line; done
+    cat offerids.txt | while read line; do docker exec $NODE curl -s -X GET http://127.0.0.1:$PORT/api/latest/payout?offer_id=$line; done
 
 done
