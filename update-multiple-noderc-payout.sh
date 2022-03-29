b1d42a30062e832174648d12d5fcf425844b6739
valcyclovir <swaggrenox@gmail.com>
2022-03-28 21:10:07 -0400

    .

---
+++ update-multiple-noderc-payout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script will update all your .origintrail_noderc files for your nodes after you made changes to your config.sh file (for instance to integrate matic blockchain)
+
+#Backwards compatibility to old config location
+if [ -f "/etc/otnode/config.sh" ]; then
+  source "/etc/otnode/config.sh"
+else
+  #**Deprecated** Move config to /etc/otnode/config.sh and change paths in that file
+  source "/root/OT-2Nodes1Server/config.sh"
+  export MAINPATH="/root/OT-2Nodes1Server"
+fi
+
+for (( i=$NODE_COUNTER; i<=$NODE_TOTAL; i++ ))
+do
+  echo "Setting up origintrail RC file for $NODE"
+  $MAINPATH/data/setup-noderc-payout.sh $i
+
+done
+
+echo ".origintrail_noderc file updated for all nodes ! Make sure you restart your nodes to apply the changes."
