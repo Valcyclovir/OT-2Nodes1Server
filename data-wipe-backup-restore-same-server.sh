a75a5ce7dbb82b3392e03d907ee21760766a2f8a
valcyclovir <swaggrenox@gmail.com>
2022-04-07 21:19:43 -0400

    .

--- data-wipe-backup-restore-same-server.sh
+++ data-wipe-backup-restore-same-server.sh
@@ -15,6 +15,35 @@ fi
 source "$MAINPATH/data/fixed-variables.sh"
 
 N1=$'\n'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+BOLD='\e[1m'
+UNBOLD='\e[0m'
+
+shopt -s nocasematch
+
+echo_color() {
+    echo -e "$1$2$NC"
+}
+
+echo_header() {
+    echo && echo_color "$YELLOW $1" && echo
+}
+
+perform_step() {
+    echo -n "$2: "
+    OUTPUT=$($1 2>&1)
+    if [[ $? -ne 0 ]]; then
+        echo_color $RED "FAILED"
+        echo -e "${N1}Step failed. Output of error is:${N1}${N1}$OUTPUT"
+        exit 1
+    else
+        echo_color $GREEN "OK"
+    fi
+}
 
 for var
 do
@@ -23,32 +52,17 @@ do
   PORT3=$((var+8899))
   NODE="$NODE_NAME$var"
   
-  echo "rm -rf $NODEBASEPATH/temp$var"
-  rm -rf $NODEBASEPATH/temp$var
-
-  echo "mkdir $NODEBASEPATH/temp$var"
-  mkdir $NODEBASEPATH/temp$var
-  
-  echo "docker start $NODE"
-  docker start $NODE
+  perform_step "rm -rf $NODEBASEPATH/temp$var" "Removing old temp path"
+  perform_step "mkdir $NODEBASEPATH/temp$var" "Making new temp path directory"
+  perform_step "docker start $NODE" "Start $NODE"
 
   sleep 4s
 
-  echo "docker cp $NODE:/ot-node/data/identity.json $NODEBASEPATH/temp$var/"
-  docker cp $NODE:/ot-node/data/identity.json $NODEBASEPATH/temp$var/
-
-  echo "docker cp $NODE:/ot-node/data/erc725_identity.json $NODEBASEPATH/temp$var/"
-  docker cp $NODE:/ot-node/data/erc725_identity.json $NODEBASEPATH/temp$var/
-
-  echo "docker cp $NODE:/ot-node/data/xdai_erc725_identity.json $NODEBASEPATH/temp$var/"
-  docker cp $NODE:/ot-node/data/xdai_erc725_identity.json $NODEBASEPATH/temp$var/
-
-  echo "docker cp $NODE:/ot-node/data/polygon_erc725_identity.json $NODEBASEPATH/temp$var/"
-  docker cp $NODE:/ot-node/data/polygon_erc725_identity.json $NODEBASEPATH/temp$var/
-
-  echo "docker stop $NODE"
-  docker stop $NODE
-
+  perform_step "docker cp $NODE:/ot-node/data/identity.json $NODEBASEPATH/temp$var/" "Copying identity.json"
+  perform_step "docker cp $NODE:/ot-node/data/erc725_identity.json $NODEBASEPATH/temp$var/" "Copying erc725_identity.json"
+  perform_step "docker cp $NODE:/ot-node/data/xdai_erc725_identity.json $NODEBASEPATH/temp$var/" "Copying xdai_erc725_identity.json"
+  perform_step "docker cp $NODE:/ot-node/data/polygon_erc725_identity.json $NODEBASEPATH/temp$var/" "Copying polygon_erc725_identity.json"
+  perform_step "docker stop $NODE" "Stopping $NODE"
 done
 
 while true; do
@@ -61,8 +75,7 @@ while true; do
   esac
 done
 
-echo "docker rm -f $NODE"
-docker rm -f $NODE
+perform_step "docker rm -f $NODE" "Deleting $NODE"
 
 for var
 do
@@ -71,46 +84,28 @@ do
   PORT3=$((var+8899))
   NODE="$NODE_NAME$var"
 
-  echo "Setting up Firewall rules"
-  ufw allow $PORT1 && ufw allow $PORT2 && ufw allow $PORT3
+  perform_step "ufw allow $PORT1 && ufw allow $PORT2 && ufw allow $PORT3" "Setting up Firewall rules"
+  
+  perform_step "rm -rf $NODEBASEPATH/$NODE" "Deleting old RC file"
 
-  echo "removing old $NODEBASEPATH/$NODE and making new directory $NODEBASEPATH/$NODE"
-  rm -rf $NODEBASEPATH/$NODE
-  mkdir $NODEBASEPATH/$NODE
+  perform_step "mkdir $NODEBASEPATH/$NODE" "Making new RC file directory"
 
-  echo "Setting up origintrail RC file for $NODE"
-  $MAINPATH/data/setup-noderc.sh $var
+  perform_step "$MAINPATH/data/setup-noderc.sh $var" "Setting up origintrail RC file for $NODE"
+
+  perform_step "docker create -i --log-driver json-file --log-opt max-size=50m --name=$NODE -p $PORT3:$PORT3 -p $PORT2:$PORT2 -p $PORT1:$PORT1 -v $NODEBASEPATH/$NODE/.origintrail_noderc:/ot-node/.origintrail_noderc origintrail/ot-node:release_mainnet" "Creating new $NODE"
 
   ID_IMPORT=false
-
 	for (( t=0; t<3; t++ ))
 	do
-    echo "Creating $NODE"
-    OUTPUT=$(docker create -i --log-driver json-file --log-opt max-size=50m --name=$NODE -p $PORT3:$PORT3 -p $PORT2:$PORT2 -p $PORT1:$PORT1 -v $NODEBASEPATH/$NODE/.origintrail_noderc:/ot-node/.origintrail_noderc origintrail/ot-node:release_mainnet 2>&1)
-    if [[ $? -ne 0 ]]; then
-      echo "Docker creation FAILED:${N1}$OUTPUT"
-      exit 1
-    fi
-
-    echo "docker start $NODE"
-    docker start $NODE
-    if [[ $? -ne 0 ]]; then
-      echo "Docker start FAILED"
-      exit 1
-    fi
+    perform_step "docker start $NODE" "Starting $NODE"
 
     sleep 5s
 
-    echo "docker stop $NODE"
-    docker stop $NODE
-    if [[ $? -ne 0 ]]; then
-      echo "Docker stop FAILED"
-      exit 1
-    fi
+    perform_step "docker stop $NODE" "Stopping $NODE"
 
     sleep 1s
 
-    echo "mv $NODEBASEPATH/temp$var/* $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data/"
+    echo "Moving identities to $NODE"
     mv $NODEBASEPATH/temp$var/* $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data/
     if [[ $? -eq 0 ]]; then
       #success, stop trying
@@ -128,24 +123,9 @@ do
     exit 1
   fi
 
-  chmod -R 777 $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data
-  if [[ $? -ne 0 ]]; then
-    echo "chmod -R 777 /ot-node/data for $NODE failed !"
-    exit 1
-  fi
-
-  chown -R root:root $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data
-  if [[ $? -ne 0 ]]; then
-    echo "chown -R root:root /ot-node/data for $NODE failed !"
-    exit 1
-  fi
-
-  echo "docker start $NODE"
-  docker start $NODE
-  if [[ $? -ne 0 ]]; then
-    echo "docker start $NODE failed"
-    exit 1
-  fi
+  perform_step "chmod -R 777 $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data" "Setting chmod to $NODE"
+  
+  perform_step "chown -R root:root $($DOCKER_INSPECT_UPPER $NODE)/ot-node/data" "Setting chown to $NODE"
 
   sleep 3s
 
@@ -168,10 +148,9 @@ do
     sed -i 's/command=arangod.*/command=arangod --rocksdb.write-buffer-size '$write_buffer_size' --rocksdb.total-write-buffer-size '$total_write_buffer_size' --rocksdb.max-write-buffer-number '$max_write_buffer_number' --rocksdb.dynamic-level-bytes '$dynamic_level_bytes' --rocksdb.block-cache-size '$block_cache_size' --server.statistics '$server_statistics' /g' $($DOCKER_INSPECT_MERGED $NODE)/ot-node/$version/testnet/supervisord.conf
   fi
 
-  docker update --memory=5G --memory-swap=10G $NODE
-  
-  echo "docker restart $NODE"
-  docker restart $NODE
+  perform_step "docker update --memory=5G --memory-swap=10G $NODE" "Added swap to $NODE"
+
+  perform_step "docker restart $NODE" "Restarting $NODE"
 
 done
 
